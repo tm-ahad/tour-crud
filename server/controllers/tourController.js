4c9a34524c12e6da3ef7f43f1adcb8b1ea2dd03c
tm-ahad <tm.ahad.07@gmail.com>
2022-07-17 10:36:34 +0600

    Tour updated succsessfully

--- server/controllers/tourController.js
+++ server/controllers/tourController.js
@@ -1,6 +1,7 @@
 import User from "../models/user.js";
 import p from "../models/tour.js";
 import jwt from 'jsonwebtoken';
+import tokenizer from "../auth/auth.js";
 
 const { Tour } = p;
 
@@ -13,22 +14,37 @@ const tourController = {
       delete dbObj.exp;
       await User.findOneAndUpdate({email: user.email}, {...dbObj, Tours: [...dbObj.Tours, tour]})
       res.status(201).send('Tour created on user succsessfully');
+      await User.findOne({ email: user.email }, (err, findedUser) => {
+         cookies.token = tokenizer(findedUser);
+      });
    },
    cancelTour: async ({ body: { id }, cookies }, res) => {
-      const user = jwt.decode(cookies.token);
-      const dbObj = {...user};
-      delete dbObj.iat;
-      delete dbObj.exp;
-      delete dbObj.Tours;
-      User.findOne({ email: user.email }).updateOne({}, { 
+      const { email } = jwt.decode(cookies.token);
+      User.findOne({ email }).updateOne({}, { 
          $pull: { Tours: { _id: id } }
        }).then(() => res.status(200).send('Tour deleted😎'))
+       User.findOne({ email }, (err, findedUser) => {
+         cookies.token = tokenizer(JSON.parse(JSON.stringify(findedUser)));
+      });
    },
-   updateTour({ req, res }){
-      const user = jwt.decode(cookies.token);
-      this.cancelTour(req, res);
-      this.bookTour(req, res);
-      res.status(201).send('Tour updated on user succsessfully');
+   updateTour: async ({ body: { index, update }, cookies }, res) => {
+      const { email } = jwt.decode(cookies.token);
+      User.findOne({ email }, {}, {}, (err, doc) => {
+          if (err){
+            throw err;
+          } else {
+            const { name, age, email, password, accountName, Tours: userTours } = doc;
+            userTours[index] = new Tour(update);
+            User.findOne({ email }).updateOne({ 
+               name,
+               age, 
+               email,
+               password,
+               accountName, 
+               Tours: userTours
+            }).then(() => res.status(200).send('Op!'));
+         }
+      })
    },
 };
 export default tourController;
